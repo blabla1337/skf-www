4d2dc0be14a88e6f709e9cc18eeb5dc0c75dabe0
Riccardo ten Cate <r.tencate77@gmail.com>
2015-06-21 17:49:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 //Terugzetten na dns fix
-//header('Strict-Transport-Security: max-age=16070400; includeSubDomains');
+header('Strict-Transport-Security: max-age=16070400; includeSubDomains');
 header('X-Frame-Options: deny');
 header('X-XSS-Protection: 1; mode=block');
 header('X-Content-Type-Options: nosniff');
